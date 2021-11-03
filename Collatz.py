8c15e41d74e8f317aa3c579aa8b38a9923b95908
Drama_Lvl1 <53279807+DramaLvl1@users.noreply.github.com>
2021-11-03 20:27:03 +0100

    Update Collatz.py

--- Collatz.py
+++ Collatz.py
@@ -1,16 +1,23 @@
 # Programm Code für Collatz Problem
-a = input("Number input")
+
+a = input("Zahl eingeben")
 i = 0
+
+if len(str(a)) > 20:
+  print("Eine Zahl mit mehr als 22 Ziffern zeigt nur Errors ;)")
+  a = 1
+
+
 
 while a != 1:
   if int(a) % 2 == 1:
     a = int(a) * 3 + 1
     i += 1
-    print("Number: " + str(a))
+    print("Zahl: " + str(a))
   
   elif int(a) % 2 == 0:
     a = int(a) / 2
     i += 1
-    print("Number: " + str(a))
+    print("Zahl: " + str(a))
 
-print("The number has been calculated " + str(i) + " times")
+print("Es wurden " + str(i) + " Rechnungen durchgeführt")
